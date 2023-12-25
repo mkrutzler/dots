5de19b96a42f306ce97429ac8867983081bf297e
Mark Krutzler <116576496+mkrutzler@users.noreply.github.com>
2023-12-25 10:40:06 +0100

    Add dwm configs via upload

---
+++ dwm/colors.h
@@ -0,0 +1,6 @@
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+
+};
