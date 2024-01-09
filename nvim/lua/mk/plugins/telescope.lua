c1c2dc857a398fbda6b9140e4ae42265fe9ba0ba
Mark Krutzler <mark.krutzler@icloud.com>
2024-01-09 05:25:27 +0100

    enabled color scheme

--- nvim/lua/mk/plugins/telescope.lua
+++ nvim/lua/mk/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
 		config = function()
 			require("telescope").setup({})
 			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>pf', builtin.find_files )
 			vim.keymap.set('n', '<leader>pws', function()
 				local word = vim.fn.expand("<cword>")
 				builtin.grep_string({ seach = word })
